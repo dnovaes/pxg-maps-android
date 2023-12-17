da787cebd2e9ef87f1e4f574816cb3dbfdfd0f29
Diego Novaes <diego.novaes@MacBook-Pro-de-Diego.local>
2023-12-17 01:59:31 -0300

    Updates compile and target sdk to v34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.dnovaes.pxgmapsandroid"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.dnovaes.pxgmapsandroid"
         minSdk = 28
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
